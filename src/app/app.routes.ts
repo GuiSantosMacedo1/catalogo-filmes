360db6141259c580e4ccf56cda5fc637a8875f26
GuiSantosMacedo1 <guisantosmacedo7@gmail.com>
2026-07-21 10:19:09 -0300

    feat: adicionar componente de detalhes do filme com template e estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { MovieDetailComponent } from './features/movie-detail/movie-detail.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'filme/:id', component: MovieDetailComponent }
 ];
